5004177bc8f77f9b328f774215339579a1bb648a
cesar-pz <cesar.pz@berkeley.edu>
2022-03-15 13:59:00 -0700

    Kivy starter code

---
+++ KivyTemplate/main.py
@@ -0,0 +1,167 @@
+import os
+import sys
+
+# os.environ['DISPLAY'] = ":0.0"
+# os.environ['KIVY_WINDOW'] = 'egl_rpi'
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+from pidev.MixPanel import MixPanel
+from pidev.kivy.PassCodeScreen import PassCodeScreen
+from pidev.kivy.PauseScreen import PauseScreen
+from pidev.kivy import DPEAButton
+from pidev.kivy import ImageButton
+
+sys.path.append("/home/soft-dev/Documents/dpea-odrive/")
+from odrive_helpers import *
+
+MIXPANEL_TOKEN = "x"
+MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
+
+SCREEN_MANAGER = ScreenManager()
+MAIN_SCREEN_NAME = 'main'
+TRAJ_SCREEN_NAME = 'traj'
+GPIO_SCREEN_NAME = 'gpio'
+ADMIN_SCREEN_NAME = 'admin'
+
+
+class ProjectNameGUI(App):
+    """
+    Class to handle running the GUI Application
+    """
+
+    def build(self):
+        """
+        Build the application
+        :return: Kivy Screen Manager instance
+        """
+        return SCREEN_MANAGER
+
+
+Window.clearcolor = (1, 1, 1, 1)  # White
+
+
+class MainScreen(Screen):
+    """
+    Class to handle the main screen and its associated touch events
+    """
+
+    def switch_to_traj(self):
+        SCREEN_MANAGER.transition.direction = "left"
+        SCREEN_MANAGER.current = TRAJ_SCREEN_NAME
+
+    def switch_to_gpio(self):
+        SCREEN_MANAGER.transition.direction = "right"
+        SCREEN_MANAGER.current = GPIO_SCREEN_NAME
+
+
+    def admin_action(self):
+        """
+        Hidden admin button touch event. Transitions to passCodeScreen.
+        This method is called from pidev/kivy/PassCodeScreen.kv
+        :return: None
+        """
+        SCREEN_MANAGER.current = 'passCode'
+
+
+class TrajectoryScreen(Screen):
+    """
+    Class to handle the trajectory control screen and its associated touch events
+    """
+
+    def switch_screen(self):
+        SCREEN_MANAGER.transition.direction = "right"
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+
+
+class GPIOScreen(Screen):
+    """
+    Class to handle the GPIO screen and its associated touch/listening events
+    """
+
+    def switch_screen(self):
+        SCREEN_MANAGER.transition.direction = "left"
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+
+
+class AdminScreen(Screen):
+    """
+    Class to handle the AdminScreen and its functionality
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Load the AdminScreen.kv file. Set the necessary names of the screens for the PassCodeScreen to transition to.
+        Lastly super Screen's __init__
+        :param kwargs: Normal kivy.uix.screenmanager.Screen attributes
+        """
+        Builder.load_file('AdminScreen.kv')
+
+        PassCodeScreen.set_admin_events_screen(
+            ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
+        PassCodeScreen.set_transition_back_screen(
+            MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
+
+        super(AdminScreen, self).__init__(**kwargs)
+
+    @staticmethod
+    def transition_back():
+        """
+        Transition back to the main screen
+        :return:
+        """
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+
+    @staticmethod
+    def shutdown():
+        """
+        Shutdown the system. This should free all steppers and do any cleanup necessary
+        :return: None
+        """
+        os.system("sudo shutdown now")
+
+    @staticmethod
+    def exit_program():
+        """
+        Quit the program. This should free all steppers and do any cleanup necessary
+        :return: None
+        """
+        quit()
+
+
+"""
+Widget additions
+"""
+
+Builder.load_file('main.kv')
+SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(TrajectoryScreen(name=TRAJ_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(GPIOScreen(name=GPIO_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
+SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
+SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
+
+"""
+MixPanel
+"""
+
+
+def send_event(event_name):
+    """
+    Send an event to MixPanel without properties
+    :param event_name: Name of the event
+    :return: None
+    """
+    global MIXPANEL
+
+    MIXPANEL.set_event_name(event_name)
+    MIXPANEL.send_event()
+
+
+if __name__ == "__main__":
+    # send_event("Project Initialized")
+    # Window.fullscreen = 'auto'
+    ProjectNameGUI().run()
--- docs/getting_started/getting_started.md
+++ docs/getting_started/getting_started.md
@@ -11,3 +11,43 @@ has_children: true
 1. TOC
 {:toc}
 ---
+
+## Intro
+TBD
+
+## Using this Library in Python
+After you have followed the [Getting Started section in ODrive Docs](https://docs.odriverobotics.com/v/latest/getting-started.html)
+and have successfully controlled your motor using `odrivetool`, it is time to start using the
+[odrive_helpers](https://github.com/dpengineering/dpea-odrive/blob/main/odrive_helpers.py) library.
+This library will allow you to automate the process of using `odrivetool` by bundling many of the commands in Python.
+
+For example, to move the motor one revolution in `odrivetool` we would need to do the following,
+``` 
+odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.controller.input_pos = 1
+```
+
+But using the `odrive_helpers` library we would simply need to run a command such as
+```python
+axis0.calibrate()
+axis0.set_pos(1)
+```
+
+To experiment with the `odrive_helpers` library, run `ipython3` and follow along the various 
+[examples](/dpea-odrive/examples/examples) in the sidebar.
+
+## Kivy GUI
+After you have tested each example, let us finish by making a Kivy GUI. You will find template files in the `KivyTemplate`
+folder of this repository. Your goal with this GUI is to have the following --
+* A button that toggles between moving the motor 5 rotations clockwise and counterclockwise
+* A slider that controls the velocity of the motor
+* A second slider that controls the acceleration of the motor (i.e. two sliders to handle ramped velocity)
+* Button that homes the motor, either to position 0, or until a wall is hit, whichever comes first.
+* Another screen that controls the motor with trapezoidal trajectory control. 
+  Have text boxes for acceleration, target position, and deceleration plus a submit button to send the command to the motor.
+* Another screen which utilizes a GPIO pin to move the motor when a sensor or switch is activated.
+
+
+
+
