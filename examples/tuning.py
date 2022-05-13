ce94c6decf6ae656a282be9b0cc0600a14dc563e
cesar-pz <cesar.pz@berkeley.edu>
2022-05-13 15:37:05 -0700

    added watchdog and tuning examples

---
+++ examples/tuning.py
@@ -0,0 +1,65 @@
+"""
+Run this program in a terminal setting to tune your odrive motors.
+The program will return the tuned values for pos_gain, vel_gain, and vel_integrator_gain.
+This has not been extensively tested, thus do not over-rely on the values returned here. Use as a starting point.
+"""
+import sys
+
+sys.path.append("/home/soft-dev/Documents/dpea-odrive")
+
+from odrive_helpers import *
+from odrive.utils import start_liveplotter
+from time import sleep
+
+if __name__ == "__main__":
+    od = find_odrive()
+
+    assert od.config.enable_brake_resistor is True, "Check for faulty brake resistor."
+
+    ax = ODriveAxis(od.axis0)
+    ax.set_gains()
+
+    if not ax.is_calibrated():
+        print("calibrating...")
+        ax.calibrate()
+        print("Done Calibrating")
+    else:
+        print("Motor Already Calibrated.")
+
+    try:
+        ax.axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        print("Now Attempting to Tune Motor")
+        ax.set_vel_integrator_gain(0)
+        while input("Is System Stable? [y/n]") == "n":
+            ax.set_gains(ax.get_pos_gain() / 2, ax.get_vel_gain() / 2, ax.get_vel_integrator_gain() / 2)
+
+        print("Attempting vel_gain Tuning.")
+        sleep(1)
+        while input("Is Motor Vibrating? [y/n]") == "n":
+            ax.set_vel_gain(ax.get_vel_gain() * 1.3)
+
+        ax.set_vel_gain(ax.get_vel_gain() * .5)
+        print("Attempting pos_gain Tuning.")
+        start_liveplotter(lambda: [ax.axis.encoder.pos_estimate, ax.axis.controller.input_pos])
+        sleep(5)
+        print("Pay Attention For Any Overshoot")
+        ax.set_relative_pos(1)
+        ax.wait_for_motor_to_stop()
+        ax.set_relative_pos(-1)
+        while input("Is There Overshoot? [y/n]") == "n":
+            ax.set_pos_gain(ax.get_pos_gain() * 1.3)
+            ax.set_relative_pos(1)
+            sleep(1)
+            ax.set_relative_pos(-1)
+            sleep(1)
+        ax.set_pos_gain(ax.get_pos_gain() / 1.3)
+
+        ax.set_vel_integrator_gain(.5 * 10 * ax.get_vel_gain())
+        print("Done Calibrating")
+
+    finally:
+        ax.idle()
+        print(f"Pos Gain: {ax.get_pos_gain()}, Vel Gain: {ax.get_vel_gain()}, Vel Int Gain: {ax.get_vel_integrator_gain()}")
+        dump_errors(od)
+        od.clear_errors()
+        print("DONE")
---
+++ examples/watchdog_test.py
@@ -0,0 +1,43 @@
+"""
+Example program of using ODrive's built-in watchdog to handle cutting current to the motor when communication is lost.
+"""
+import sys
+
+sys.path.append("/home/soft-dev/Documents/dpea-odrive")
+
+from odrive_helpers import *
+from odrive.utils import start_liveplotter
+from odrive.enums import AXIS_ERROR_WATCHDOG_TIMER_EXPIRED
+from time import sleep
+
+if __name__ == "__main__":
+    od = find_odrive()
+
+    assert od.config.enable_brake_resistor is True, "Check for faulty brake resistor."
+
+    ax = ODriveAxis(od.axis0)
+    if not ax.is_calibrated():
+        print("calibrating...")
+        ax.calibrate()
+    ax.set_gains()
+    # Example from odrive repo:
+    # https://github.com/odriverobotics/ODrive/blob/48433c61c6a00a1edae33a37674966aad3bcabb0/tools/odrive/tests/test_runner.py#L181
+    ax.axis.config.enable_watchdog = False
+    ax.axis.error = 0
+    ax.axis.config.watchdog_timeout = .5  # if watchdog hasn't been fed in over .5 seconds, motor idles
+    ax.axis.watchdog_feed()
+    ax.axis.config.enable_watchdog = True
+
+    try:
+        while True: # the 'action' loop, part of code that is talking to the motor and continuously feeding watchdog
+            ax.set_vel(2)
+            ax.axis.watchdog_feed()  # feed the watchdog to reset the watchdog_timeout.
+            sleep(.1)
+
+    finally:
+        # ax.axis.config.enable_watchdog = False
+        ax.idle()
+        if ax.axis.error == AXIS_ERROR_WATCHDOG_TIMER_EXPIRED:
+            ax.axis.error = 0
+        dump_errors(od)
+        print("DONE")
